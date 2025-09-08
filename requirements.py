58f505e524b8aa05e8b6ed36055b44a16a9bfe68
Jaluan Newson <jnewson@catalyte.io>
2025-09-08 10:07:32 -0500

    fixed bruce and tiffany

---
+++ requirements.py
@@ -0,0 +1,16 @@
+fred_steal = 10000
+kiera_steal= 1/4 * fred_steal  
+billy_bob_steal = 1000 + kiera_steal 
+jessica_steal = 8 * billy_bob_steal 
+nigel_steal = 2 * jessica_steal 
+greg_steal = 7 * kiera_steal 
+bruce_steal = jessica_steal - 2000
+tiffany_steal = bruce_steal - 7000 
+nicholas_steal = bruce_steal / 2  
+peter_steal = tiffany_steal / 10  
+
+
+sum_of_stealing = fred_steal + kiera_steal + billy_bob_steal + jessica_steal + nigel_steal+ greg_steal + bruce_steal + tiffany_steal + nicholas_steal + peter_steal 
+
+
+print(sum_of_stealing) 
