f01a084d98ba92d332a4fa365b5e91547e96fdd6
Jaluan Newson <jnewson@catalyte.io>
2025-09-10 10:12:35 -0500

    initial commit

--- requirements.py
+++ requirements.py
@@ -1,16 +1,21 @@
-fred_steal = 10000
-kiera_steal= 1/4 * fred_steal  
-billy_bob_steal = 1000 + kiera_steal 
-jessica_steal = 8 * billy_bob_steal 
-nigel_steal = 2 * jessica_steal 
-greg_steal = 7 * kiera_steal 
-bruce_steal = jessica_steal - 2000
-tiffany_steal = bruce_steal - 7000 
-nicholas_steal = bruce_steal / 2  
-peter_steal = tiffany_steal / 10  
+num_list1 = [1,2,3,4,5,6]
+
+num_list2 = [16,23,4,3,45,7]
+
+print(num_list1, num_list2)
+
+total_sum = 0
+
+for number in num_list1: 
+    total_sum += number 
+
+print({total_sum})
+
+for number in num_list2: 
+    total_sum += number 
+
+print({total_sum})
 
 
-sum_of_stealing = fred_steal + kiera_steal + billy_bob_steal + jessica_steal + nigel_steal+ greg_steal + bruce_steal + tiffany_steal + nicholas_steal + peter_steal 
+    
 
-
-print(sum_of_stealing) 
